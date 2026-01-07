ce0b08ec7efa2f4779893f05f1fd2c77b032cadd
Alex Lee <3076438032@qq.com>
2026-01-07 16:50:23 +0800

    feat: nfs logic

--- frontend/providers/devbox/constants/devbox.ts
+++ frontend/providers/devbox/constants/devbox.ts
@@ -249,7 +249,9 @@ export enum YamlKindEnum {
   Service = 'Service',
   Ingress = 'Ingress',
   Issuer = 'Issuer',
-  Certificate = 'Certificate'
+  Certificate = 'Certificate',
+  ConfigMap = 'ConfigMap',
+  PersistentVolumeClaim = 'PersistentVolumeClaim'
 }
 
 export enum PodStatusEnum {
